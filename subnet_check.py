a81309f140191d5026fef4c66e954a4da69d3144
schubb2003 <33440404+schubb2003@users.noreply.github.com>
2019-02-13 10:29:38 -0500

    Update subnet_check.py
    Additional comments and basic connectivity error checking added.

--- subnet_check.py
+++ subnet_check.py
@@ -1,14 +1,18 @@
 #!/usr/local/bin/python
 """
-# Author: Scott Chubb scott.chubb@netapp.com
-# Written for Python 3.4 and above
-# No warranty is offered, use at your own risk.  While these scripts have been tested in lab situations, all use cases cannot be accounted for.
-# MVIP and username are required, password is not as getpass is invoked if password is blank
-# This script was created to be able to compare host access IPs to the SVIP virtual network and determine if the connections were local or routed
-# As of v1, it only works on clusters with a single virtual network and has no error checking.
+Author: Scott Chubb scott.chubb@netapp.com
+Date:	13-Feb-2019
+Written for Python 3.4 and above
+No warranty is offered, use at your own risk.  While these scripts have been tested in lab situations, all use cases cannot be accounted for.
+MVIP and username are required, password is not as getpass is invoked if password is blank
+This script was created to be able to compare host access IPs to the SVIP virtual network and determine if the connections were local or routed
+As of v1, it only works on clusters with a single virtual network and has no error checking
+	If you have multiple VLANs the script only checks the first one. 
+v1.1 - added basic connectivity error checking and updated some comments to help identify what is happening where
 """
 
 import argparse
+#from pprint import pprint
 from getpass import getpass
 from solidfire.factory import ElementFactory
 
@@ -43,11 +47,27 @@ def get_inputs():
 
 def connect_cluster():
 	"""
-	Actually connect to the cluster
+	Attempt to connect to the cluster
 	"""
 	sfmvip, sfuser, sfpass = get_inputs()
-	sfe = ElementFactory.create(sfmvip,sfuser,sfpass, print_ascii_art=False)
-	return sfe
+	try:
+		sfe = ElementFactory.create(sfmvip,sfuser,sfpass, print_ascii_art=False)
+		return sfe
+	except Exception as ex:
+		if "Bad Credentials" in str(ex):
+			print("Incorrect user or password entered, please re-enter:\n")
+			sfuser = input("Enter user name: ")
+			sfpass = getpass("Enter password for user {} on cluster {}: ".format(sfuser, sfmvip))
+			sfe = ElementFactory.create(sfmvip,sfuser,sfpass, print_ascii_art=False)
+			return sfe
+		elif "host has failed to respond" in str(ex):
+			sfmvip = input("Please re-enter MVIP: ")
+			sfe = ElementFactory.create(sfmvip,sfuser,sfpass, print_ascii_art=False)
+			return sfe
+		else:
+			print("Script will exit due to an unhandled exception: \n{}".format(str(ex)))
+			exit()
+			
 
 def find_net_info(sfe):
 	"""
@@ -57,14 +77,17 @@ def find_net_info(sfe):
 	print("-" * 20 + " find_net_info started")
 	virt_net = sfe.list_virtual_networks()
 	json_virt_net = virt_net.to_json()
+	#pprint(json_virt_net)
 	virt_mask = json_virt_net['virtualNetworks'][0]['netmask']
 	svip = json_virt_net['virtualNetworks'][0]['svip']
 
+	# Break the netmask into constituent octets to get the one that determines the host network
 	mask_oct1 = int(virt_mask.split(".")[0])
 	mask_oct2 = int(virt_mask.split(".")[1])
 	mask_oct3 = int(virt_mask.split(".")[2])
 	mask_oct4 = int(virt_mask.split(".")[3])
 
+	# Return the octet that has the determining bits
 	if mask_oct1 != 255:
 		oct_pos = 0
 		comp_oct = mask_oct1
@@ -78,8 +101,10 @@ def find_net_info(sfe):
 		oct_pos = 3
 		comp_oct = mask_oct4
 
+	# Find the network block size
 	comp_block = 256 - comp_oct 
 
+	# Find the SVIP host bits
 	comp_svip = int(svip.split(".")[oct_pos])
 	int_svip = int(comp_svip)
 	return int_svip, comp_block, oct_pos
@@ -93,11 +118,13 @@ def find_block(int_svip, comp_block):
 	outsz = 0
 	bsdict = {}
 	bsdict [0] = bsz
+	# Build the dictionary of the host networks
 	while outsz < 255:
 		outsz = outsz + bsz
 		bsdict[outsz] = (outsz + bsz) -1
 		#print(outsz)
-		
+	
+	# Determine the upper and lower bounds of the host network
 	for key in bsdict.keys():
 		if int_svip >= key and int_svip <= bsdict[key]:
 			block_start = key
@@ -134,9 +161,10 @@ def main(oct_pos):
 		ip = ip_port.split(":")[0]
 		ip_array.append(ip)
 
+	# Determine if the host and the cluster are in the same host network or are connecting via a routed network
 	for ip_comp in ip_array:
 		determine_oct = int(ip_comp.split(".")[oct_pos])
-		#print(block_start, determine_oct, block_end)
+		print(block_start, determine_oct, block_end)
 		if determine_oct  >= block_start and determine_oct <= block_end:
 			print("Pass, {} is in the right subnet".format(ip_comp))
 		else:
